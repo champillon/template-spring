c00baac968db5f3678113749fb3859503496b7e0
Passapong Thaithatgoon <champillon@rurouni.local>
2021-11-18 10:03:39 +0700

    added GET /system/version from pom.xml

--- src/main/java/demo/pt/templatespring/TemplateSpringApplication.java
+++ src/main/java/demo/pt/templatespring/TemplateSpringApplication.java
@@ -1,13 +1,32 @@
 package demo.pt.templatespring;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TemplateSpringApplication {
+
+	@Autowired
+	BuildProperties buildProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateSpringApplication.class, args);
+	}
+
+	@GetMapping("/system/version")
+	@PostConstruct
+	public String version() {
+		final var appName = buildProperties.getName();
+		final var appVersion = buildProperties.getVersion();
+
+		return "application: " + appName + " version: " + appVersion;
 	}
 
 }
