51e268f214c74bbde87fd71f6ac63a19d592dfca
Passapong Thaithatgoon <champillon@rurouni.local>
2021-11-18 09:25:41 +0700

    initialized spring boot project

---
+++ src/test/java/demo/pt/templatespring/TemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.pt.templatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
