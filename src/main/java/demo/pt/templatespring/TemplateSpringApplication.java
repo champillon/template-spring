f580192798362e004dae31c1612e31c2095ae882
Passapong Thaithatgoon <champillon@rurouni.local>
2021-11-18 10:11:27 +0700

    removed unused import

--- src/main/java/demo/pt/templatespring/TemplateSpringApplication.java
+++ src/main/java/demo/pt/templatespring/TemplateSpringApplication.java
@@ -1,13 +1,7 @@
 package demo.pt.templatespring;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.info.BuildProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TemplateSpringApplication {
