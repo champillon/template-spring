8e56d448bf3e4f57e2b72e62a6152ff14fa04d4f
Passapong Thaithatgoon <champillon@rurouni.local>
2023-07-04 08:29:14 +0700

    added cal dotnet WeatherForecast

--- src/main/java/demo/pt/templatespring/controller/SystemController.java
+++ src/main/java/demo/pt/templatespring/controller/SystemController.java
@@ -7,8 +7,10 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/system")
@@ -25,8 +27,13 @@ public class SystemController {
 		final String appName = buildProperties.getName();
 		final String appVersion = buildProperties.getVersion();
 		final String result = "application: " + appName + " version: " + appVersion;
+		String uri = "http://104.208.78.166:7272/WeatherForecast";
 
-		logger.info(result);
+        RestTemplate restTemplate = new RestTemplate();
+        String weatherForecast = restTemplate.getForObject(uri, String.class);
+
+        logger.info(result+" "+weatherForecast);
+		
 		return result;
 	}
     
